afe29dd3854391ff197a9af45df9c08921be3a8f
Devon Poon Woo <devon.poonwoo@gmail.com>
2021-03-06 19:17:59 -0500

    Added Game Over Screen, Enemy Spawning.

---
+++ ILP2-Unity/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    int maxEnemies;
+
+    float prevSpawnTime = -1;
+
+    [SerializeField]
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject Enemy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkSpawn();
+    }
+
+    void checkSpawn()
+    {
+        if (Time.time - prevSpawnTime >= spawnInterval) {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        prevSpawnTime = Time.time;
+
+        Vector3 position;
+
+        int randNum = Random.Range(0, 2);
+
+        if (randNum == 0) {
+            //spawn at top of screen
+            position = new Vector3(Random.Range(-10f, 10f), 5.5f, 0);
+        }
+        else {
+            //spawn at bottom of screen
+            position = new Vector3(Random.Range(-10f, 10f), -5.5f, 0);
+        }
+
+        Instantiate(Enemy, position, Quaternion.identity);
+
+    }
+}
---
+++ ILP2-Unity/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField]
+    byte maxEnemyPool;
+
+    GameObject[] Enemies;
+    int[] emptyIndexes;//stores list of inactive objects indexes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemies = new GameObject[maxEnemyPool];
+        emptyIndexes = new int[maxEnemyPool];
+
+        foreach (GameObject enemy in Enemies) {
+            enemy.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void destroyObject(int index)
+    {
+        Enemies[index].SetActive(false);
+    }
+
+    void spawnObject()
+    {
+
+    }
+}
